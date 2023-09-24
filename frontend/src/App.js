5a9bc179826dc063707438d30a10b6001724baf7
aadhar2003 <68272092+aadhar2003@users.noreply.github.com>
2023-09-24 19:05:45 +0530

    comit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom"
-import Login from './pages/login/Login'
+// import Login from './pages/login/Login'
 import FarmerHome from './pages/home/FarmerHome'
 import './App.css'
 import Profile from './pages/profile/Profile'
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
     <Routes>
-    <Route path="/" element={<Login />} />
+    {/* <Route path="/" element={<Login />} /> */}
     <Route path="/farmerhome" element={<FarmerHome />} />
     <Route path="/profile" element={<Profile />} />
     </Routes>
