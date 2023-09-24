d485074345a844e6c3ffa8aac38e1d4f2a2d6a99
aadhar2003 <68272092+aadhar2003@users.noreply.github.com>
2023-09-24 19:10:37 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import { Route, Routes } from "react-router-dom"
 import FarmerHome from './pages/home/FarmerHome'
 import './App.css'
 import Profile from './pages/profile/Profile'
+import Login from "./pages/Log/Login"
 
 const App = () => {
   return (
     <>
     <Routes>
-    {/* <Route path="/" element={<Login />} /> */}
+    <Route path="/" element={<Login />} />
     <Route path="/farmerhome" element={<FarmerHome />} />
     <Route path="/profile" element={<Profile />} />
     </Routes>
